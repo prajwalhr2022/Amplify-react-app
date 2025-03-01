9dc85d9962c0f33e739e391761155bb5406780af
Prajwal_H_R <prajjugowda856@gmail.com>
2025-03-01 12:28:44 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
-import Quiz from "./Quiz";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+import Quiz from "./Quiz";
+
+// Configure Amplify
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
-      <h1>React Quiz Game</h1>
-      <Quiz />
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <header className="App-header">
+              <h2>Welcome, {user?.username}!</h2>
+              <Quiz />
+              <button 
+                onClick={signOut} 
+                style={{ 
+                  margin: "20px", 
+                  fontSize: "0.8rem", 
+                  padding: "5px 10px", 
+                  marginTop: "20px"
+                }}
+              >
+                Sign Out
+              </button>
+            </header>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
 
+// No need for withAuthenticator
 export default App;
